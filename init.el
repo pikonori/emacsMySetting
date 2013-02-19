ccd9fe5782ecbae3b9da724922c6acd2ff81e24e
hirabaru <hirabaru@n-create.co.jp>
2013-02-19 09:31:30 +0900

    バックアップファイルを一箇所にまとめる

--- init.el
+++ init.el
@@ -273,3 +273,13 @@
 
 (require 'auto-highlight-symbol)
 (global-auto-highlight-symbol-mode t)
+
+
+;;バックアップファイルの保存先変更
+
+(setq make-backup-files t)
+;;; バックアップファイルの保存場所を指定。
+(setq backup-directory-alist
+      (cons (cons "\\.*$" (expand-file-name "~/.emacs.d/backup"))
+            backup-directory-alist))
+
