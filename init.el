7a932226eb340573d95b43c053b1dd0f6d852394
hirabaru <hirabaru@n-create.co.jp>
2013-03-01 18:02:37 +0900

    coffee-mode導入

--- init.el
+++ init.el
@@ -283,3 +283,14 @@
       (cons (cons "\\.*$" (expand-file-name "~/.emacs.d/backup"))
             backup-directory-alist))
 
+
+;; coffee-mode
+(require 'coffee-mode)
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (and (set (make-local-variable 'tab-width) 2)
+       (set (make-local-variable 'coffee-tab-width) 2))
+  )
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
